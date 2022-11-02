826567890f71326be3bf73271cdced4351d9085d
Rohit <ramteker284@gmail.com>
2022-11-02 16:16:48 +0530

    v1.0.4

--- site/js/navbar.js
+++ site/js/navbar.js
@@ -2,14 +2,19 @@ const menu_btn = document.getElementById('menu-btn');
 const navtop = document.querySelector('.navtop');
 const navitems = document.querySelector('.nav-items');
 
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
 
 // first set the display to none
-navitems.style.display = 'none';
-menu_btn.addEventListener('click', ()=> {
-    if(navitems.style.display == 'block') {
-        navitems.style.display = 'none';
-    } else {
-        navitems.style.display = 'block';
-    }
-})
-
+console.log(screenWidth, screenHeight);
+if(screenWidth <= 449)
+{
+    navitems.style.display = 'none';
+    menu_btn.addEventListener('click', ()=> {
+        if(navitems.style.display == 'block') {
+            navitems.style.display = 'none';
+        } else {
+            navitems.style.display = 'block';
+        }
+    })
+}
--- site/js/theme.js
+++ site/js/theme.js
@@ -2,6 +2,8 @@ const themeToggler = document.getElementById('theme-btn');
 
 const container = document.querySelector('.container');
 
+
+
 themeToggler.addEventListener('click', () => {
     container.classList.toggle('theme');
 })
